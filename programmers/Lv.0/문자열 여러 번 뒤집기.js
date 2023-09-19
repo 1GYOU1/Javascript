e9c02d515aa67e06135679a48ffec5a7d37f13d3
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2023-09-19 23:41:49 +0900

    2023-09-19 알고리즘

---
+++ programmers/Lv.0/문자열 여러 번 뒤집기.js
@@ -0,0 +1,48 @@
+/*
+<문자열 여러 번 뒤집기>
+
+문제 설명
+문자열 my_string과 이차원 정수 배열 queries가 매개변수로 주어집니다. queries의 원소는 [s, e] 형태로, my_string의 인덱스 s부터 인덱스 e까지를 뒤집으라는 의미입니다. my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+my_string은 영소문자로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+queries의 원소는 [s, e]의 형태로 0 ≤ s ≤ e < my_string의 길이를 만족합니다.
+1 ≤ queries의 길이 ≤ 1,000
+입출력 예
+my_string	queries	result
+"rermgorpsam"	[[2, 3], [0, 7], [5, 9], [6, 10]]	"programmers"
+입출력 예 설명
+예제 1번의 my_string은 "rermgorpsam"이고 주어진 queries를 순서대로 처리하면 다음과 같습니다.
+
+queries	my_string
+"rermgorpsam"
+[2, 3]	"remrgorpsam"
+[0, 7]	"progrmersam"
+[5, 9]	"prograsremm"
+[6, 10]	"programmers"
+따라서 "programmers"를 return 합니다.
+*/
+
+//나의 풀이
+const solution = (my_string, queries) => {
+    let newArr = my_string.split('');
+    let slice = [];
+    let result = [];
+    for(let i=0;i<queries.length;i++){
+        slice = newArr.splice(queries[i][0], queries[i][1]-queries[i][0]+1)
+        slice.reverse();
+        for(let s=0;s<slice.length;s++){
+            newArr.splice(queries[i][0]+s, 0, slice[s])
+            result = newArr;
+        }
+    }
+    return result.join('')
+}
+
+/*
+    my_string 잘라서 배열로 만들어두고,
+    해당 배열에서 필요한 부분을 잘라오는데, splice로 잘라서 newArr 자체에서 빼냈다.
+    그리고 reverse로 순서 바꿔서 slice 변수에 저장.
+    잘라둔 newArr 원본 배열에 다시 slice를 알맞은 자리에 splice로 넣어서 join('');
+*/
